6571448d18b192d69d17a8a1e9562962e194a3c4
ShotaLaabb <shotikoslabadzes1@gmail.com>
2026-06-04 17:06:21 +0400

    feat: ready for vercel deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+
+export const createNestServer = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+  return server;
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  createNestServer().then(() => {
+    server.listen(process.env.PORT ?? 3000, () => {
+      console.log('Running on http://localhost:3000');
+    });
+  });
 }
-bootstrap();
+
+export default createNestServer;
